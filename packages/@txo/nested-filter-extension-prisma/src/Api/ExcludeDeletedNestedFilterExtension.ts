13bff6ddf8949d45ccb1aa15da695388b7fa8e42
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2021-04-24 18:43:52 +0200

    Add initial code

---
+++ packages/@txo/nested-filter-extension-prisma/src/Api/ExcludeDeletedNestedFilterExtension.ts
@@ -0,0 +1,32 @@
+/**
+ * @Author: Rostislav Simonik <rostislav.simonik@technologystudio.sk>
+ * @Date: 2021-04-18T15:04:77+02:00
+ * @Copyright: Technology Studio
+**/
+
+import { Extension, ExtensionOptions, Condition } from '@txo-peer-dep/nested-filter-prisma'
+
+type Options = {
+  excludeDeleted?: boolean,
+}
+
+declare module '@txo-peer-dep/nested-filter-prisma' {
+  interface ExtensionOptions {
+    excludeDeleted: boolean,
+  }
+}
+
+export class ExcludedDeletedNestedFilterExtension implements Extension {
+  _defaultOptions?: Options
+  constructor (defaultOptions?: Options) {
+    this._defaultOptions = defaultOptions
+  }
+
+  populateConditionList = (conditionList: Condition[], extensionOptions?: ExtensionOptions): void => {
+    if (extensionOptions?.excludeDeleted ?? this._defaultOptions?.excludeDeleted) {
+      conditionList.push({
+        deletedDateTime: null,
+      })
+    }
+  }
+}
--- packages/@txo/nested-filter-extension-prisma/src/Api/index.ts
+++ packages/@txo/nested-filter-extension-prisma/src/Api/index.ts
@@ -1,7 +1,7 @@
 /**
  * @Author: Rostislav Simonik <rostislav.simonik@technologystudio.sk>
- * @Date: 2021-04-18T15:04:81+02:00
+ * @Date: 2021-04-18T15:04:62+02:00
  * @Copyright: Technology Studio
 **/
 
-export * from './Api'
+export * from './ExcludeDeletedNestedFilterExtension'
